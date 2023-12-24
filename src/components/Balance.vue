6e7b37860e26ec350909b0573c1978f70ebd0b61
Mohammed Taqi <taqealibade@gmail.com>
2023-12-24 19:10:09 +0300

    delete some css test borders 😃

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class=" border-2 border-red-500 w-full h-full text-center px-4 my-6 xs:py-6 lg:flex lg:flex-col">
-    <h4 class="text-white text-2xl border-2 lg-mb-4">Your Balance</h4>
-    <h1 class=" border-2 mt-6 text-white text-3xl lg:mb-2" id="balance">${{ total }}</h1>
+    <div class=" w-full h-full text-center px-4 my-6 xs:py-6 lg:flex lg:flex-col">
+    <h4 class="text-white text-2xl lg-mb-4">Your Balance</h4>
+    <h1 class="mt-6 text-white text-3xl lg:mb-2" id="balance">${{ total }}</h1>
 </div>
 </template>
 
